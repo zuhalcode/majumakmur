481d73ecf38a87f5a9ab9aceabc4291ace1f97a6
zuhalcode <zuhalcode@gmail.com>
2026-09-05 10:15:53 +0700

    fix(asset): remove console log from asset balance response

--- src/features/asset/services/asset-balance.service.ts
+++ src/features/asset/services/asset-balance.service.ts
@@ -4,7 +4,6 @@ import { AssetBalance } from "../types/asset.types";
 export const assetBalanceService = {
   async findAll(): Promise<{ data: AssetBalance[] }> {
     const res = await api.get("/asset-balances");
-    console.log(res.data);
     return res.data;
   },
 };
